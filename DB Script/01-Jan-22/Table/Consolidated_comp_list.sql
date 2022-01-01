a6d4e09e850b4bfceccb3b2d52decba7dddb97aa
Venkatesh.G <venkatg.07it@gmail.com>
2022-01-01 12:13:14 +0530

    dummy po adjust table script added

---
+++ DB Script/01-Jan-22/Table/Consolidated_comp_list.sql
@@ -0,0 +1,55 @@
+USE [IndustriesDB]
+GO
+
+/****** Object:  Table [dbo].[ConsolidatedComponentList]    Script Date: 01-Jan-22 12:12:43 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[ConsolidatedComponentList](
+	[ComponentListIdentity] [int] IDENTITY(1,1) NOT NULL,
+	[Itemcode] [varchar](100) NULL,
+	[description] [varchar](150) NULL,
+	[RMCode] [varchar](100) NULL,
+	[RMDescription] [varchar](150) NULL,
+	[lc1] [int] NULL,
+	[lc2] [varchar](1) NULL,
+	[Size1] [varchar](50) NULL,
+	[Size2] [varchar](50) NULL,
+	[Size3] [varchar](50) NULL,
+	[FinalQty] [int] NULL,
+	[process1] [varchar](30) NULL,
+	[process2] [varchar](30) NULL,
+	[process3] [varchar](30) NULL,
+	[process4] [varchar](30) NULL,
+	[process5] [varchar](30) NULL,
+	[process6] [varchar](30) NULL,
+	[process7] [varchar](30) NULL,
+	[process8] [varchar](30) NULL,
+	[process9] [varchar](30) NULL,
+	[process10] [varchar](30) NULL,
+	[process11] [varchar](30) NULL,
+	[process12] [varchar](30) NULL,
+	[process13] [varchar](30) NULL,
+	[process14] [varchar](30) NULL,
+	[process15] [varchar](30) NULL,
+	[finishtype] [varchar](75) NULL,
+	[surfacepreparation] [varchar](75) NULL,
+	[primercolour] [varchar](75) NULL,
+	[primerDFT] [varchar](75) NULL,
+	[IMOColour] [varchar](75) NULL,
+	[IMODFT] [varchar](75) NULL,
+	[TopCoatColour] [varchar](75) NULL,
+	[TopCoatDFT] [varchar](75) NULL,
+	[remarks] [varchar](500) NULL,
+	[LoadDay] [varchar](25) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[ComponentListIdentity] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
